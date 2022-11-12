1d33eb522b297f40174592fa43a9d92ee0fcc25b
hazre <mail@haz.re>
2022-11-12 23:03:15 +0100

    fix: temp fix pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,35 @@
 <script>
 	import '../app.css';
 
-	import { onMount } from 'svelte';
-	import { pwaInfo } from 'virtual:pwa-info';
+	//	import { onMount } from 'svelte';
+	// 	import { pwaInfo } from 'virtual:pwa-info';
 
-	onMount(async () => {
-		if (pwaInfo) {
-			const { registerSW } = await import('virtual:pwa-register');
-			registerSW({
-				immediate: true,
-				onRegistered(r) {
-					// uncomment following code if you want check for updates
-					// r && setInterval(() => {
-					//    console.log('Checking for sw update')
-					//    r.update()
-					// }, 20000 /* 20s for testing purposes */)
-					console.log(`SW Registered: ${r}`);
-				},
-				onRegisterError(error) {
-					console.log('SW registration error', error);
-				}
-			});
-		}
-	});
+	// 	onMount(async () => {
+	// 		if (pwaInfo) {
+	// 			const { registerSW } = await import('virtual:pwa-register');
+	// 			registerSW({
+	// 				immediate: true,
+	// 				onRegistered(r) {
+	// 					// uncomment following code if you want check for updates
+	// 					// r && setInterval(() => {
+	// 					//    console.log('Checking for sw update')
+	// 					//    r.update()
+	// 					// }, 20000 /* 20s for testing purposes */)
+	// 					console.log(`SW Registered: ${r}`);
+	// 				},
+	// 				onRegisterError(error) {
+	// 					console.log('SW registration error', error);
+	// 				}
+	// 			});
+	// 		}
+	// 	});
 
-	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+	// 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+	//
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	{@html webManifest}
-</svelte:head>
+</svelte:head> -->
 
 <slot />
