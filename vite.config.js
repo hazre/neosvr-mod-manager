bd543dcb3097c40fac18267638bda05281662d1c
hazre <mail@haz.re>
2022-11-14 22:03:45 +0100

    wip: trying to get vite pwa to work

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import Icons from 'unplugin-icons/vite';
-// import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
@@ -9,39 +9,39 @@ const config = {
 		Icons({
 			autoInstall: true,
 			compiler: 'svelte'
+		}),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			strategies: 'generateSW',
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+				navigateFallback: null
+			},
+			manifest: {
+				lang: 'en-us',
+				short_name: 'NeosVR Mod Manager',
+				name: 'NeosVR Mod Manager',
+				start_url: '/',
+				description: 'Easily install NeosVR mods and maintain them with ease.',
+				scope: '/',
+				display: 'standalone',
+				theme_color: '#111827',
+				background_color: '#111827',
+				display_override: ['window-controls-overlay'],
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
 		})
-		// SvelteKitPWA({
-		// 	registerType: 'autoUpdate',
-		// 	devOptions: {
-		// 		enabled: true,
-		// 		type: 'module',
-		// 		navigateFallback: '/'
-		// 	},
-		// 	manifest: {
-		// 		lang: 'en-us',
-		// 		short_name: 'NeosVR Mod Manager',
-		// 		name: 'NeosVR Mod Manager',
-		// 		start_url: '/',
-		// 		description: 'Easily install NeosVR mods and maintain them with ease.',
-		// 		scope: '/',
-		// 		display: 'standalone',
-		// 		theme_color: '#111827',
-		// 		background_color: '#111827',
-		// 		display_override: ['window-controls-overlay'],
-		// 		icons: [
-		// 			{
-		// 				src: '/pwa-192x192.png',
-		// 				sizes: '192x192',
-		// 				type: 'image/png'
-		// 			},
-		// 			{
-		// 				src: '/pwa-512x512.png',
-		// 				sizes: '512x512',
-		// 				type: 'image/png'
-		// 			}
-		// 		]
-		// 	}
-		// })
 	]
 };
 
