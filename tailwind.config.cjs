c50ccc79dbdaf28f537425c37e7594c2c8dbd309
hazre <mail@haz.re>
2022-11-16 17:47:00 +0100

    build(deps): add flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 	theme: {
 		extend: {}
-	}
+	},
+	plugins: [require('flowbite/plugin')]
 };
